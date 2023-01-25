cb31986010aec57d8187f193bd1c05c8869c7061
Learn010 <geenbagger+github@gmail.com>
2023-01-25 22:09:03 +0100

    buttons added

--- gui.py
+++ gui.py
@@ -4,10 +4,16 @@ import PySimpleGUI as sg
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="enter todo", key="todo")
 add = sg.Button("add")
+list_box = sg.Listbox(values=functions.get_todos(), key="todos",
+                      enable_events=True, size=[45, 10])
+
+edit = sg.Button("edit")
 
 
 window = sg.Window("test",
-                   layout=[[label], [input_box, add]],
+                   layout=[[label],
+                           [input_box, add],
+                           [list_box, edit] ],
                    font=('helvetica',20))
 while True:
     event, values = window.read()
@@ -19,7 +25,18 @@ while True:
             new_todo = values['todo'] + "\n"
             todos.append(new_todo)
             functions.write_todos(todos)
+            window['todos'].update(values=todos)
+        case "edit":
+            todo_to_edit = values['todos'][0]
+            new_todo = values['todo']
 
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo+"\n"
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+        case "todos":
+            window['todo'].update(value=values['todos'][0])
         case sg.WIN_CLOSED:
             break
 
