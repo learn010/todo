a076fa6453122db218736f341d02798f14ecdb1b
Learn010 <geenbagger+github@gmail.com>
2023-01-26 18:35:27 +0100

    complete and exit button added

--- extra/exercizes/exercize16.py
+++ extra/exercizes/exercize16.py
@@ -1,15 +1,30 @@
 import PySimpleGUI as sg
 
+def convert(feet, inches):
+    meters = feet * 0.3048 + inches *  0.0254
+    return meters
+
+
+
 ft_label = sg.Text("Enter feet: ")
 inch_label = sg.Text("Enter inches: ")
-input_ft = sg.InputText()
-input_inch = sg.InputText()
+input_ft = sg.InputText(key="feet")
+input_inch = sg.InputText(key="inches")
 conv_button = sg.Button("Convert")
+result_label = sg.Text("",key="result_label")
+
 
 window = sg.Window("convertor",
                    layout=[[ft_label,input_ft],
                            [inch_label, input_inch],
-                           [conv_button]])
+                           [conv_button,result_label]])
 
-window.read()
+while True:
+   event, values = window.read()
+   print(f"event ={event} values = {values}")
+   match event:
+       case Convert:
+           result = convert(float(values['feet']),float(values['inches']))
+           window['result_label'].update(value=f"this is {result} meters")
+
 window.close()
--- gui.py
+++ gui.py
@@ -1,19 +1,23 @@
 import functions
 import PySimpleGUI as sg
 
+
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="enter todo", key="todo")
 add = sg.Button("add")
 list_box = sg.Listbox(values=functions.get_todos(), key="todos",
                       enable_events=True, size=[45, 10])
 
-edit = sg.Button("edit")
+edit = sg.Button("edit", key="edit")
+complete = sg.Button("complete", key="complete")
+exit_button = sg.Button("exit")
 
 
 window = sg.Window("test",
                    layout=[[label],
                            [input_box, add],
-                           [list_box, edit] ],
+                           [list_box, edit, complete],
+                           [exit_button]],
                    font=('helvetica',20))
 while True:
     event, values = window.read()
@@ -26,15 +30,24 @@ while True:
             todos.append(new_todo)
             functions.write_todos(todos)
             window['todos'].update(values=todos)
+            window['todo'].update(value="")
         case "edit":
             todo_to_edit = values['todos'][0]
             new_todo = values['todo']
-
             todos = functions.get_todos()
             index = todos.index(todo_to_edit)
             todos[index] = new_todo+"\n"
             functions.write_todos(todos)
             window['todos'].update(values=todos)
+        case "complete":
+            todo_to_complete = values['todos'][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window['todos'].update(valuae=todos)
+            window['todo'].update(value="")
+        case "exit":
+            break
         case "todos":
             window['todo'].update(value=values['todos'][0])
         case sg.WIN_CLOSED:
