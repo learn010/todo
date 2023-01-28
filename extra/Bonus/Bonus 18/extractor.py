0beb255ec8c501a62deef02bc1d49aebb0c8051b
Learn010 <geenbagger+github@gmail.com>
2023-01-28 10:17:18 +0100

    extract thing

--- extra/Bonus/Bonus 18/extractor.py
+++ extra/Bonus/Bonus 18/extractor.py
@@ -1,2 +1,9 @@
 import zipfile
 
+def extractor(inputfile,extract_dir):
+    with zipfile.ZipFile(inputfile, 'r') as file:
+        file.extractall(extract_dir)
+
+
+if __name__ == "__main__":
+    extractor("/home/paul/Downloads/compressed.zip","/home/paul/PycharmProects/Pythonmegacourse/extra/Bonus/Bonus 18")
--- extra/Bonus/Bonus 18/main.py
+++ extra/Bonus/Bonus 18/main.py
@@ -1,15 +1,22 @@
 import PySimpleGUI as sg
+import extractor
 
-source = sg.FilesBrowse("source", key="source_file", size=10)
+source = sg.FileBrowse("source", key="source_file", size=10)
 destination = sg.FolderBrowse("Destination",key="destination", size=10)
 input_box_source = sg.InputText(key="source_path")
 input_box_dest = sg.InputText(key="dest_path")
 extract = sg.Button("extract", key="extract")
-status = sg.Text("")
+status = sg.Text("", key= "status")
 
 window = sg.Window("File Extractor", layout=[[],[input_box_source, source],
                                              [input_box_dest, destination],
                                              [extract, status]])
+while True:
+    event, value = window.read()
+    print(event,value)
+    source_file = value['source_file']
+    dest_loc = value['destination']
+    extractor.extractor(source_file,dest_loc)
+    window['status'].update(value="Extraction Complete", text_color="Green")
 
-window.read()
 window.close()
